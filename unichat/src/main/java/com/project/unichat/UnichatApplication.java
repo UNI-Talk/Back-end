8e3b5ef8fa9e62363f4cfad5baf0467592c1fa9c
noah <ssh7636@gmail.com>
2022-03-15 22:33:08 +0900

    project init

---
+++ unichat/src/main/java/com/project/unichat/UnichatApplication.java
@@ -0,0 +1,13 @@
+package com.project.unichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnichatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnichatApplication.class, args);
+    }
+
+}
---
+++ unichat/src/test/java/com/project/unichat/UnichatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.unichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnichatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
